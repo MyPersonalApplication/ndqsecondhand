d2f8414757af8fbc8f11e5f58baaa6b3a802fd7b
quangndgcc200030 <91590378+quangndgcc200030@users.noreply.github.com>
2023-05-19 15:15:46 +0700

    upadate database

--- connectdatabase.php
+++ connectdatabase.php
@@ -1,3 +1,3 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'ndqsecondhand')
+    $conn = mysqli_connect('us-cdbr-east-06.cleardb.net', 'bbc1204aad28d6', 'ff42b70b', 'heroku_dd751b011d7a466')
             or die("Can not conncect database".mysqli_connect_error());
